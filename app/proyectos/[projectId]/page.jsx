994323295142150bb65e180b476e8a4cb7c2208c
Yubal <ydefuente@gmail.com>
2024-09-02 22:16:11 +0200

    deploy test

--- app/proyectos/[projectId]/page.jsx
+++ app/proyectos/[projectId]/page.jsx
@@ -1,11 +1,5 @@
 import { projectsDetailsData } from "@/data/data";
-import AnimatedEntrance from "@/src/components/AnimatedEntrance/AnimatedEntrance";
-import BackButton from "@/src/components/BackButton/BackButton";
-import Card from "@/src/components/Card/Card";
-import DescriptionText from "@/src/components/DescriptionText/DescriptionText";
-import Title from "@/src/components/Title/Title";
-import Image from "next/image";
-import { notFound } from "next/navigation";
+import ProjectComponent from "@/src/components/ProjectComponent/ProjectComponent";
 
 export async function generateStaticParams() {
     return projectsDetailsData.map((project) => ({
@@ -22,49 +16,10 @@ function Proyecto({ params }) {
     if (!project) {
         return notFound();
     }
-    // const { setLoading } = useLoading();
-    // useEffect(() => {
-    //     setLoading(false);
-    // }, []);
 
     return (
         <section>
-            <div className="flex flex-col items-center p-5 gap-24">
-                <div className="flex flex-col gap-8 mt-[15vh] md:self-start md:ml-12 items-start">
-                    <AnimatedEntrance
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                    >
-                        <BackButton />
-                    </AnimatedEntrance>
-                    <AnimatedEntrance delay={0.2}>
-                        <Title>{project.name}</Title>
-                    </AnimatedEntrance>
-                    <AnimatedEntrance delay={0.4}>
-                        <DescriptionText>{project.description}</DescriptionText>
-                    </AnimatedEntrance>
-                </div>
-                <div
-                    className="hero-project"
-                    style={{
-                        backgroundImage: `radial-gradient(circle farthest-side at 50% 0, ${project.color}, #0000)`,
-                        opacity: 0.6,
-                    }}
-                ></div>
-                <div className="flex gap-8">
-                    {project.images.map((image, index) => (
-                        <Card key={index} className={"overflow-hidden"}>
-                            <div className="h-[200px] w-[200px]">
-                                <Image 
-                                    src={image}
-                                    alt="project image"
-                                    fill
-                                />
-                            </div>
-                        </Card>
-                    ))}
-                </div>
-            </div>
+            <ProjectComponent project={project} />
         </section>
     );
 }
---
+++ src/components/ProjectComponent/ProjectComponent.jsx
@@ -0,0 +1,54 @@
+"use client";
+import AnimatedEntrance from "@/src/components/AnimatedEntrance/AnimatedEntrance";
+import BackButton from "@/src/components/BackButton/BackButton";
+import Card from "@/src/components/Card/Card";
+import DescriptionText from "@/src/components/DescriptionText/DescriptionText";
+import Title from "@/src/components/Title/Title";
+import { useLoading } from "@/src/context/LoadingContext";
+import Image from "next/image";
+import { useEffect } from "react";
+
+function ProjectComponent({ project }) {
+    const { setLoading } = useLoading();
+
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    return (
+            <div className="flex flex-col items-center p-5 gap-24">
+                <div className="flex flex-col gap-8 mt-[15vh] md:self-start md:ml-12 items-start">
+                    <AnimatedEntrance
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                    >
+                        <BackButton />
+                    </AnimatedEntrance>
+                    <AnimatedEntrance delay={0.2}>
+                        <Title>{project.name}</Title>
+                    </AnimatedEntrance>
+                    <AnimatedEntrance delay={0.4}>
+                        <DescriptionText>{project.description}</DescriptionText>
+                    </AnimatedEntrance>
+                </div>
+                <div
+                    className="hero-project"
+                    style={{
+                        backgroundImage: `radial-gradient(circle farthest-side at 50% 0, ${project.color}, #0000)`,
+                        opacity: 0.6,
+                    }}
+                ></div>
+                <div className="flex gap-8">
+                    {project.images.map((image, index) => (
+                        <Card key={index} className={"overflow-hidden"}>
+                            <div className="h-[200px] w-[200px]">
+                                <Image src={image} alt="project image" fill />
+                            </div>
+                        </Card>
+                    ))}
+                </div>
+            </div>
+    );
+}
+
+export default ProjectComponent;
